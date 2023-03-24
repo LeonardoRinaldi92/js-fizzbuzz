f0b93dd7f38733e282fbd9e66520fbbbcae159c2
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-24 15:06:31 +0100

    COMPLETO ma si può fare di melgio!

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+for (let x = 1; x <= 150; x++) {
+    let whatAppear;
+    let css;
+  
+    if (x % 3 === 0 && x % 5 === 0) {
+      whatAppear = 'BUZZFIZZ'
+      css = 'buzzfizzStyle'
+    } else if (x % 5 === 0) {
+      whatAppear = 'FIZZ'
+      css = 'fizzStyle'
+    } else if (x % 3 === 0 ) {
+      whatAppear = 'BUZZ'
+      css = 'buzzStyle'
+    } else {
+      whatAppear = x
+      css = 'standard'
+    }
+  
+      let element = document.createElement("div");
+      element.appendChild(document.createTextNode(whatAppear ));
+      element.classList.add(css);
+      document.getElementById('buzzfizz').appendChild(element);}
